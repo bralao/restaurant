cacc1e91bec698e40e4af2d561d50d959b5e843b
bralao <bernralao@gmail.com>
2024-05-29 16:40:57 +0100

    created navbar file in components

--- client/src/layout/Main.jsx
+++ client/src/layout/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const Main = () => {
   return (
     <div>
-      <nav>Navbar</nav>
+      <Navbar/>
       <Outlet/> {/* This is where the child routes will be rendered */}
       <footer>Footer</footer>
     </div>
