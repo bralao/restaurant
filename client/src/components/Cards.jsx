6b596bd9ded35043540161817e5f65138803f870
bralao <bernralao@gmail.com>
2024-06-04 19:15:30 +0100

    customized card, included image with Link, and heart rating

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,9 +1,39 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React , { useState } from 'react'
+import { Link } from 'react-router-dom'
+import {FaHeart} from 'react-icons/fa'
 
 const Cards = ({item}) => {
+
+  const [isHeartFilled, setIsHeartFilled] = React.useState(false)
+
+  const handleHeartClick = () => {
+    setIsHeartFilled(!isHeartFilled)
+  }
+
   return (
-    <div>Cards</div>
+    <div className="card w-96 bg-base-100 shadow-xl relative">
+      <div
+        className={`heartStar rating gap-1 absolute right-2 top-2 p-4 bg-green
+          ${isHeartFilled ? "text-rose-500" : "text-white"}`}
+        onClick={handleHeartClick}
+      >
+        <FaHeart className="h-5 w-5 cursor-pointer"/>
+      </div>
+      <Link to={`/menu/${item._id}`}>
+        <figure>
+          <img src={item.image} alt="" className="hover:scale-105 transition-all duration-200 md:h-72"/>
+        </figure>
+      </Link>
+      <div className="card-body">
+        <h2 className="card-title">{item.name}</h2>
+        <p>Description of the item</p>
+        <div className="card-actions justify-between items-center mt-2">
+          <h5 className="font-semibold"><span className="text-sm text-red">$</span>{item.price}</h5>
+          <button className="btn bg-green text-white">Buy Now</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
