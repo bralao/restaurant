a7144a187cd31a882eddbf7a8f0773650d87b764
bralao <bernralao@gmail.com>
2024-06-04 19:43:12 +0100

    special dishes finished

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -12,7 +12,7 @@ const Cards = ({item}) => {
   }
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl relative">
+    <div className="card w-96 mb-7 bg-base-100 shadow-xl relative">
       <div
         className={`heartStar rating gap-1 absolute right-2 top-2 p-4 bg-green
           ${isHeartFilled ? "text-rose-500" : "text-white"}`}
@@ -26,7 +26,9 @@ const Cards = ({item}) => {
         </figure>
       </Link>
       <div className="card-body">
-        <h2 className="card-title">{item.name}</h2>
+        <Link to={`/menu/${item._id}`}>
+          <h2 className="card-title">{item.name}</h2>
+        </Link>
         <p>Description of the item</p>
         <div className="card-actions justify-between items-center mt-2">
           <h5 className="font-semibold"><span className="text-sm text-red">$</span>{item.price}</h5>
--- client/src/pages/home/SpecialDishes.jsx
+++ client/src/pages/home/SpecialDishes.jsx
@@ -3,6 +3,33 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Cards from '../../components/Cards';
+import { FaAngleRight, FaAngleLeft } from 'react-icons/fa';
+
+const simpleNextArrow = (props) => {
+  const { className, style, onClick } = props; // here we are destructuring the props, so we can use them directly in the return
+  return (
+    <div
+      className={className} // here we are using the className directly,
+      style={{...style, display: "block", background:"red"}}
+      onClick={onClick}
+    >
+      NEXT
+    </div>
+  )
+}
+
+const simplePrevArrow = (props) => {
+  const { className, style, onClick } = props; // here we are destructuring the props, so we can use them directly in the return
+  return (
+    <div
+      className={className} // here we are using the className directly,
+      style={{...style, display: "block", background:"green"}}
+      onClick={onClick}
+    >
+      BACK
+    </div>
+  )
+}
 
 const SpecialDishes = () => {
 
@@ -51,17 +78,31 @@ const SpecialDishes = () => {
           slidesToScroll: 1
         }
       }
-    ]
+    ],
+
+    nextArrow: <simpleNextArrow />,
+    prevArrow: <simplePrevArrow />
   };
 
   return (
-    <div className="section-container my-20">
+    <div className="section-container my-20 relative">
       <div className="text-left">
         <p className="subtitle">Special Dishes</p>
         <h2 className="title md:w-[520px]">Standout Dishes From Our Menu</h2>
       </div>
 
-      <Slider {...settings}>
+      {/* arrow btn */}
+      <div className="md:absolute right-3 top-8 mb-10 md:mr-24">
+        {/*here we are using the ref to call the slickNext and slickPrev methods */}
+        <button onClick={() => slider.current.slickPrev()} className="btn p-2 rounded-full ml-5">
+          <FaAngleLeft className="w-8 h-8 p-1"/>
+        </button>
+        <button onClick={() => slider.current.slickNext()} className="btn p-2 rounded-full ml-5 bg-green">
+          <FaAngleRight className="w-8 h-8 p-1"/>
+        </button>
+      </div>
+
+      <Slider ref={slider} {...settings} className="overflow-hidden mt-10 space-x-5">
         {
           recipes.map((item, i) => (
             <Cards key={i} item={item}/> // pass the item to the Cards component
