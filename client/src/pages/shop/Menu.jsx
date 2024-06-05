cfe4a1debb5b05257ef46b7e08f1788208659a23
bralao <bernralao@gmail.com>
2024-06-05 14:26:11 +0100

    defined states + loading frontend data + filtering data

--- client/src/pages/shop/Menu.jsx
+++ client/src/pages/shop/Menu.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Menu = () => {
+  const [menu, setMenu] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  // ^here, we are using useState to store the menu items and the filtered items. We are also using useState to store the selected category. This way, we can filter the menu items based on the selected category.
+  const [sortOption, setSortOption] = useState("default");
+  // ^here, we are using useState to store the sort option. This way, we can sort the menu items based on the selected sort option.
+
+  //loading data
+  useEffect(() => {
+    //fetching data from backend
+    //**fetching from frontend 1st
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/menu.json");
+        const data = await response.json(); // converting the response to json
+        /* console.log(data); */
+        setMenu(data); // setting the menu state with the fetched data
+        setFilteredItems(data); // setting the filteredItems state with the fetched data
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+    //calling the function
+    fetchData();
+  }, [])
+
+  // filtering data based on category
+  const filterItems = (category) => {
+    const filtered = category === "all"
+      ? menu
+      : menu.filter(item => item.category === category); // if the category is "all", we return all the menu items, otherwise we filter the menu items based on the selected category
+    setFilteredItems(filtered); // setting the filteredItems state with the filtered data
+    setSelectedCategory(category); // setting the selected category
+  }
+
+
   return (
     <div>
       {/* menu banner */}
