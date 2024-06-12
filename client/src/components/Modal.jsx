81fafa235338d861ef5944fce0e8e07d38cfe00f
bralao <bernralao@gmail.com>
2024-06-12 16:03:55 +0100

    a

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -3,31 +3,56 @@ import { Link } from 'react-router-dom'
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+
 
 
 const Modal = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+   } = useForm();
+
+   const onSubmit = data => console.log(data);
+
+
   return (
     <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
       <div className="modal-box">
         <div className="modal-action flex flex-col justify-center mt-0">
-          <form className="card-body" method="dialog">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body" method="dialog">
 
             <h3 className="font-bold text-lg">Please Login</h3>
 
+            {/* email */}
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="email" placeholder="email" className="input input-bordered" required />
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                {...register("email")}
+              />
             </div>
 
+            {/* password */}
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="password" placeholder="password" className="input input-bordered" required />
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                {...register("password")}
+              />
               <label className="label mt-1">
-                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
               </label>
             </div>
 
@@ -37,7 +62,11 @@ const Modal = () => {
 
             {/* login btn */}
             <div className="form-control mt-6">
-              <input type="submit"  value="Login" className="btn bg-green text-white"/>
+              <input
+                type="submit"
+                value="Login"
+                className="btn bg-green text-white"
+              />
             </div>
 
             <p className="text-center my-2">Do not have an account?
