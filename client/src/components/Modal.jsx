bdd827976923fdde1b0cef3397dbdf52a2aa5a9c
bralao <bernralao@gmail.com>
2024-06-12 12:53:18 +0100

    modal created

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+
+const Modal = () => {
+  return (
+    <dialog id="my_modal_5" className="modal modal-middle sm:modal-middle">
+      <div className="modal-box">
+        <div className="modal-action flex flex-col justify-center mt-0">
+          <form className="card-body" method="dialog">
+
+            <h3 className="font-bold text-lg">Please Login</h3>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="email" placeholder="email" className="input input-bordered" required />
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="password" placeholder="password" className="input input-bordered" required />
+              <label className="label mt-1">
+                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+              </label>
+            </div>
+
+            {/* error text */}
+
+
+
+            {/* login btn */}
+            <div className="form-control mt-6">
+              <input type="submit"  value="Login" className="btn bg-green text-white"/>
+            </div>
+
+            <p className="text-center my-2">Do not have an account?
+              <Link className="underline text-red ml-1" to="/signup">Signup Now</Link>
+            </p>
+
+
+
+          </form>
+
+          {/* social login */}
+          <div className="text-center space-x-3 mb-5">
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaGoogle />
+            </button>
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaFacebookF />
+            </button>
+            <button className="btn btn-circle hover:bg-green hover:text-white">
+              <FaGithub />
+            </button>
+
+          </div>
+
+        </div>
+      </div>
+    </dialog>
+
+  )
+}
+
+export default Modal
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import logo from '/logo.png' // no need to write the hole path because it is in the public folder
 import { FaRegUser } from "react-icons/fa";
+import Modal from './Modal';
 
 
 const Navbar = () => {
@@ -79,10 +80,12 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end" >
+
           {/* search button */}
           <button className="btn btn-ghost btn-circle hidden lg:flex">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
           </button>
+
           {/*cart items */}
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle mr-3 lg:flex hidden items-center justify-center ">
             <div className="indicator">
@@ -90,11 +93,16 @@ const Navbar = () => {
               <span className="badge badge-sm indicator-item">8</span>
             </div>
           </div>
-          {/*btn */}
-          <button className="btn bg-green rounded-full px-6 text-white flex items-center gap-2">
+
+          {/*login btn */}
+          <button
+          onClick={()=>document.getElementById('my_modal_5').showModal()}
+          className="btn bg-green rounded-full px-6 text-white flex items-center gap-2">
             <FaRegUser />
             Login
           </button>
+
+          <Modal />
         </div>
       </div>
     </header>
